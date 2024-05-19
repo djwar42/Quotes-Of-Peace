37deea51736d87fa04c6b2a6e24d427ff92b4b05
Daniel <happyrobotstudio@icloud.com>
2024-05-19 22:07:48 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const getBaseUrl = () => {
   if (process.env.NODE_ENV === 'development') {
     return 'http://localhost:3000'
   } else {
-    return process.env.NEXT_PUBLIC_BASE_URL
+    return process.env.BASE_URL
   }
 }
 
